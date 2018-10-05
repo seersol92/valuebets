0642e3b423a19d925f26d8884f1a5643b91523f6
seersol92 <hamad.pixiders@gmail.com>
2018-10-05 17:37:10 +0500

    indent

--- comparing.php
+++ comparing.php
@@ -1,100 +1,98 @@
 <?php include 'templates/header.php'; ?>
 <main class="main">
   <!-- Breadcrumb-->
-  <ol class="breadcrumb">
+    <ol class="breadcrumb">
     <li class="breadcrumb-item">Home</li>
     <li class="breadcrumb-item active">Dashboard</li>
     <!-- Breadcrumb Menu-->
-  </ol>
-  <div class="container-fluid">
-    <div class="animated fadeIn"> 
-
-      <div class="row">
-          <div class="col-sm-6 col-lg-3">
+    </ol>
+        <div class="container-fluid">
+        <div class="animated fadeIn"> 
+        <div class="row">
+            <div class="col-sm-6 col-lg-3">
             <div class="card text-white bg-primary">
-              <div class="card-body pb-0">
+                <div class="card-body pb-0">
                 <div class="btn-group float-right">
-                  <button class="btn btn-transparent dropdown-toggle p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <button class="btn btn-transparent dropdown-toggle p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="icon-settings"></i>
-                  </button>
-                  <div class="dropdown-menu dropdown-menu-right">
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right">
                     <a class="dropdown-item" href="#">Action</a>
                     <a class="dropdown-item" href="#">Another action</a>
                     <a class="dropdown-item" href="#">Something else here</a>
-                  </div>
+                    </div>
                 </div>
                 <div class="text-value">9.823</div>
                 <div>Members online</div>
-              </div>
-              <div class="chart-wrapper mt-3 mx-3" style="height:70px;">
+                </div>
+                <div class="chart-wrapper mt-3 mx-3" style="height:70px;">
                 <canvas class="chart" id="card-chart1" height="70"></canvas>
-              </div>
+                </div>
             </div>
-          </div>
-          <!-- /.col-->
-          <div class="col-sm-6 col-lg-3">
+            </div>
+            <!-- /.col-->
+            <div class="col-sm-6 col-lg-3">
             <div class="card text-white bg-info">
-              <div class="card-body pb-0">
+                <div class="card-body pb-0">
                 <button class="btn btn-transparent p-0 float-right" type="button">
-                  <i class="icon-location-pin"></i>
+                    <i class="icon-location-pin"></i>
                 </button>
                 <div class="text-value">9.823</div>
                 <div>Members online</div>
-              </div>
-              <div class="chart-wrapper mt-3 mx-3" style="height:70px;">
+                </div>
+                <div class="chart-wrapper mt-3 mx-3" style="height:70px;">
                 <canvas class="chart" id="card-chart2" height="70"></canvas>
-              </div>
+                </div>
             </div>
-          </div>
-          <!-- /.col-->
-          <div class="col-sm-6 col-lg-3">
+            </div>
+            <!-- /.col-->
+            <div class="col-sm-6 col-lg-3">
             <div class="card text-white bg-warning">
-              <div class="card-body pb-0">
+                <div class="card-body pb-0">
                 <div class="btn-group float-right">
-                  <button class="btn btn-transparent dropdown-toggle p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <button class="btn btn-transparent dropdown-toggle p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="icon-settings"></i>
-                  </button>
-                  <div class="dropdown-menu dropdown-menu-right">
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right">
                     <a class="dropdown-item" href="#">Action</a>
                     <a class="dropdown-item" href="#">Another action</a>
                     <a class="dropdown-item" href="#">Something else here</a>
-                  </div>
+                    </div>
                 </div>
                 <div class="text-value">9.823</div>
                 <div>Members online</div>
-              </div>
-              <div class="chart-wrapper mt-3" style="height:70px;">
+                </div>
+                <div class="chart-wrapper mt-3" style="height:70px;">
                 <canvas class="chart" id="card-chart3" height="70"></canvas>
-              </div>
+                </div>
             </div>
-          </div>
-          <!-- /.col-->
-          <div class="col-sm-6 col-lg-3">
+            </div>
+            <!-- /.col-->
+            <div class="col-sm-6 col-lg-3">
             <div class="card text-white bg-danger">
-              <div class="card-body pb-0">
+                <div class="card-body pb-0">
                 <div class="btn-group float-right">
-                  <button class="btn btn-transparent dropdown-toggle p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <button class="btn btn-transparent dropdown-toggle p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="icon-settings"></i>
-                  </button>
-                  <div class="dropdown-menu dropdown-menu-right">
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right">
                     <a class="dropdown-item" href="#">Action</a>
                     <a class="dropdown-item" href="#">Another action</a>
                     <a class="dropdown-item" href="#">Something else here</a>
-                  </div>
+                    </div>
                 </div>
                 <div class="text-value">9.823</div>
                 <div>Members online</div>
-              </div>
-              <div class="chart-wrapper mt-3 mx-3" style="height:70px;">
+                </div>
+                <div class="chart-wrapper mt-3 mx-3" style="height:70px;">
                 <canvas class="chart" id="card-chart4" height="70"></canvas>
-              </div>
+                </div>
             </div>
-          </div>
-          <!-- /.col-->
-      </div>
-      <!-- /.row-->
-
-      <div class="row">
+            </div>
+            <!-- /.col-->
+        </div>
+        <!-- /.row-->
+        <div class="row">
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">
@@ -198,7 +196,7 @@
             </div>
         </div>
     </div>
-  </div>
+    </div>
 </main>
 
 <?php include 'templates/footer.php'; ?>
