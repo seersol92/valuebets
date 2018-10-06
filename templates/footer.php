ad723d3de731697e8488ff6d7f4f1c915e970066
seersol92 <hamad.pixiders@gmail.com>
2018-10-06 16:47:56 +0500

    footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,12 +1,13 @@
 
-  </div> <!-- closing div app-body header.php -->
+  </div> 
+  <!-- closing div app-body header.php -->
    <footer class="app-footer">
       <div>
       <p>Copyright by © <?= WEBSITE_NAME ?> <?= date('Y') ?></p>
       </div>
       
     </footer>
-    <!-- Bootstrap and necessary plugins-->
+
     <script src="assets/vendors/jquery/js/jquery.min.js"></script>
     <script src="assets/vendors/popper.js/js/popper.min.js"></script>
     <script src="assets/vendors/bootstrap/js/bootstrap.min.js"></script>
